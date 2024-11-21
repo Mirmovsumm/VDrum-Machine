c2519c171095493aa1d365566918e8766dd65390
Mirmovsumm Seyidov <email@example.com>
2024-11-21 23:30:20 +0400

    Initial commit

---
+++ src/Drum.tsx
@@ -0,0 +1,28 @@
+import { AudioClip } from "./types";
+
+interface DrumProps {
+  audioClips: AudioClip;
+}
+
+const Drum = ({ audioClip }: DrumProps) => {
+  const playSound = (clip: AudioClip) => {
+    (document.getElementById(clip.keyTrigger) as HTMLAudioElement)
+      .play()
+      .catch(console.error);
+    document.getElementById("display")!.innerHTML = clip.description;
+
+  };
+
+  return (
+    <button
+      className="drum-pad"
+      id={`drum-${audioClip.keyTrigger}`}
+      onClick={() => playSound(audioClip)}
+    >
+      <audio src={audioClip.url} id={audioClip.keyTrigger} className="clip" />
+      {audioClip.keyTrigger}
+    </button>
+  );
+};
+
+export default Drum;
